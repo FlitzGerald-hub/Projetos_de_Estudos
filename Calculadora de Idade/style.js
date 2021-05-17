5754140ee073cbdbccfdfff76672bd9ab58e6f1d
Lucimar Almeida <lucimar.a.s123@gmail.com>
2021-05-17 18:28:08 -0400

    Modificação no JS
    input "radio" esta dando erro no JS, não funcionando

--- Calculadora de Idade/style.js
+++ Calculadora de Idade/style.js
@@ -7,36 +7,48 @@ function verificar(){
     if (fano.value.lenght == 0 || fano.value > ano){
         res.innerHTML = `Dados invalidos, verifique-os!`
     }else{
-        let fsex = document.getElementsByClassName('radsex')
-        let idade = ano - fano.value
-        res.innerHTML = `de ${idade} anos`
-        if (fsex == 0){
-            //masculino
-            if (fano >= 0 && fano < 10){
-                //criança
-                res.innerHTML += 'homem'
-            }else if (fano >= 10 && fano < 18){
-                //adolecente
-                res.innerHTML += 'homem'
-            }else if (fano >= 18 && fano < 50){
-                //adulto
-                res.innerHTML += 'homem'
-            }else{
-                //idoso
-                res.innerHTML += 'homem'
-            }
-        }else{
-            //feminino
-            if (fano >= 0 && fano < 10){
-                //criança
-            }else if (fano >= 10 && fano < 18){
-                //adolecente
-            }else if (fano >= 18 && fano < 50){
-                //adulto
-            }else{
-                //idoso
-            }
+        var fsex = document.getElementsByClassName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked){
+            genero = 'Homem'
+        }else if (fsex[1].cheked){
+            genero = 'Mulher'
         }
+        /*if (fsex[0].checked){
+                //masculino
+                genero = 'Homem'
+            if (fano >= 0 && fano < 10){
+                //criança
+                
+            }else if (fano < 21){
+                //adolecente
+                
+            }else if (fano < 50){
+                //adulto
+               
+            }else{
+                //idoso
+                
+            }
+        }else if (fsex[1].checked){
+                //feminino
+                genero = 'Mulher'
+            if (fano >= 0 && fano < 10){
+                //criança
+
+            }else if (fano < 21){
+                //adolecente
+
+            }else if (fano < 50){
+                //adulto
+
+            }else{
+                //idoso
+
+            }
+        }*/
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos`
 
     }
 
