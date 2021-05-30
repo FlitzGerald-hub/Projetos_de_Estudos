9b0fde117450bdc60b0253d5c571b04939d92c04
Lucimar Almeida <lucimar.a.s123@gmail.com>
2021-05-29 21:29:18 -0400

    finalizando Projeto

--- Analisador de números/style.js
+++ Analisador de números/style.js
@@ -1,18 +1,19 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
-let res = document.querySelector('id#res')
+let res = document.querySelector('div#res')
+
 let valores = []
 
-function isNumero(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
         return true
     } else {
         return false
     }
 }
 
-function inLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
         return true
     } else {
         return false
@@ -20,12 +21,42 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('Esta tudo ok!')
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window.alert('Valor invalido ou já encontrado na lista!')
+        window.alert('Numero invalido ou já contem na lista!')
     }
-
-    
+    num.value = ''
+    num.focus()
 }
 
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} número cadastrado.</p>`
+        res.innerHTML += `<p>O maior valor é ${maior}</p>`
+        res.innerHTML += `<p>O menor valor é ${menor}</p>`
+        res.innerHTML += `<p>A soma se todos valores é${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+        
+    }
+}
